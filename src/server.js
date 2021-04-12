7042c8c9a514631353c0be05607bb055c89b230c
Tahmid Hasan <tahmid.hasan@aivalabs.com>
2021-04-12 21:26:47 +0600

    Update Route Endpoint

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/', ROUTES)
+app.use('/shopify', ROUTES)
 app.listen(config.server.port, () => {
   console.log(`App is running. URL: ${config.server.hostname}:${config.server.port}`)
 })
